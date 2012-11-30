60fb95b2f35f2dea96bb3c7f19b7552048d59aad
Zaki Akhmad <zakiakhmad@gmail.com>
2012-11-30 15:05:18 +0700

    delete the swap file

--- pymd5.py
+++ pymd5.py
@@ -1,6 +1,15 @@
 import sys
 import hashlib
 
+f = open('sample_01.wl', 'r')
+
+for line in f:
+	list.append(line)
+
+f.close()
+
+print list
+
 # shall I use dictionary or list as data structure? 
 
 #dict={}
@@ -19,19 +28,22 @@ import hashlib
 
 #print dict
 
-def read_wordlist(filename):
-	"""read the wordlist"""
-	input_file = open(filename, 'r')
-	for line in input_file:
-		print line,
-	input_file.close()
-
-def compute_md5(wordlist):
-	for wordlist in wordlists:
-		hash = hashlib.md5(line)	
-
-def main():
-	read_wordlist('sample_01.wl')	
-
-if __name__ == '__main__':
-	main()
+#def read_wordlist(filename):
+#	"""read the wordlist"""
+#	input_file = open(filename, 'r')
+#	for line in input_file:
+#		print line,
+#	input_file.close()
+#
+#def compute_md5(wordlist):
+#	for wordlist in wordlists:
+#		hash = hashlib.md5(line)	
+#
+#def insert_dict():
+#
+#
+#def main():
+#	read_wordlist('sample_01.wl')	
+#
+#if __name__ == '__main__':
+#	main()
--- trial.py
+++ trial.py
@@ -4,4 +4,6 @@ wordlist = ['admin','password']
 
 for word in wordlist:
 	md5 = hashlib.md5(word)
-	print md5.hexdigest()
+	print md5.hexdigest(), word
+
+
