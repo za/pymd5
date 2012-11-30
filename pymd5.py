6037df8b514a96251a64b51b3e950ca9b1705e12
Zaki Akhmad <zakiakhmad@gmail.com>
2012-11-30 14:25:33 +0700

    read wordlist

--- pymd5.py
+++ pymd5.py
@@ -1,7 +1,17 @@
 import hashlib
 
+#dict={}
+
 # echo the contents
+
 f = open('sample_01.wl', 'rU')
+
 for line in f:
-	print line,
+	print line,		
+	#dict[line] = 'something'	
+	#md5 = hashlib.md5(line)
+	#print line,
+	#print md5.hexdigest(),
 f.close()
+
+#print dict
--- sample_01.wl
+++ sample_01.wl
@@ -1,3 +1,2 @@
 admin
-administrator
 password
